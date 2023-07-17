c25c70afbe1caee74ba64cbe8788a53d9e169e28
Sagenthave <125151569+Sagenthave@users.noreply.github.com>
2023-07-17 14:47:24 -0400

    added files in controllers, bd schema, seeds folder

---
+++ controllers/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const userRoutes = require('./');
+const projectRoutes = require('./');
+
+router.use('/', ____Routes);
+router.use('/', _____Routes);
+
+module.exports = router;
