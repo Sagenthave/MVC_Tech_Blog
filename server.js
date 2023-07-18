7aebfc3845da2376d1abe833fe9f34d9e512b04a
Sagenthave <125151569+Sagenthave@users.noreply.github.com>
2023-07-18 17:26:33 -0400

    added to server.js to set up handlebars engine with cusome helpers and configure and link a session object with the sequelize store

--- server.js
+++ server.js
@@ -1,22 +1,49 @@
-// Dependencies
-const express = require('express');
-const exphbs = require('express-handlebars');
 const path = require('path');
-const hbs = exphbs.create({});
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
+const sequelize = require('./config/connection');
 
-// Sets up the Express App
+// Create a new sequelize store using the express-session package
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set Handlebars as the default template engine.
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
+// Configure and link a session object with the sequelize store
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+// Add express-session and store as Express.js middleware
+app.use(session(sess));
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(express.static(path.join(__dirname, 'public/css')));
-app.use(require(''));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Starts the server to begin listening
-app.listen(PORT, () => {
-  console.log('Server listening on: http://localhost:' + PORT);
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
--- utils/helper.js
+++ utils/helper.js
@@ -1,2 +1,4 @@
 
-  
+module.exports = {
+// ASK TA TO HELP
+  };
