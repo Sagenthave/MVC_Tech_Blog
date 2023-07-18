413fc73bd96c37f7088607ec083735cdc4bf1333
Sagenthave <125151569+Sagenthave@users.noreply.github.com>
2023-07-18 19:50:32 -0400

    added comment route to post a new comment

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const {Blog, Comment, User} = require('../../models');
+const { Blog } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 // CREATE A NEW BLOG POST
