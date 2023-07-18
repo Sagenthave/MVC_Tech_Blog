ab503453e3e118944a51f2ae5906bf439e6a115f
Sagenthave <125151569+Sagenthave@users.noreply.github.com>
2023-07-18 14:19:37 -0400

    models blog.js started

---
+++ models/blog.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Blog extends Model {}
+
+Blog.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        blog_title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        blog_post: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        user_id:{
+            type: DataTypes.INTEGER,
+            references:{
+                model:"user",
+                key:"id"
+            }
+        },
+    
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'blog',
+      }
+);
+
+module.exports = Blog;
